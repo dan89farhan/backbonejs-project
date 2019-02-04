0382f0257932071629a6dcfd45e746df8ede3926
ashwinidhande13 <ashdhande13@gmail.com>
2019-02-04 11:35:09 +0530

    table view changes

--- js/views/productDetail/table_view.js
+++ js/views/productDetail/table_view.js
@@ -6,6 +6,8 @@ var TableView = Backbone.View.extend({
     },
     initialize: function () {
 
+        this.model.render();
+
         // this.model.get('productDetails').on('add', this.addRow, this);
         this.model.get('productDetails').on('remove', this.removeRow, this);
 
@@ -37,4 +39,14 @@ var TableView = Backbone.View.extend({
         this.model.get('productDetails').fetch().done(function (response) {
         });
     },
+
+    render: function () {
+        this.$el.append('<div>');
+        this.$el.append('<table style="width:100%" id="table">');
+        this.$el.append('<tr>');
+        this.$el.append(' <th>name</th><th>price</th><th>brand</th><th>categories</th><th>date</th><th>type</th><th>description</th><th>action</th>');
+        this.$el.append('</tr>');
+        this.$el.append('</table>');
+        this.$el.append('</div>');
+    }
 });
